b009032c1e74288157af34b092a6577ebe41dcc0
Yuamble <52012754+Yuamble@users.noreply.github.com>
2020-03-29 00:13:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+/*setInterval(function () {
+    setTimeout(function () {
+        document.getElementById("logo_name_after_logo").textContent = "b57a9f78d";
+        setTimeout(function () {
+            document.getElementById("logo_name_after_logo").textContent = "Юамбль";
+            setTimeout(function () {
+                document.getElementById("logo_name_after_logo").textContent = "e46b198c";
+                setTimeout(function () {
+                    document.getElementById("logo_name_after_logo").textContent = "Yuamble";
+                }, 150);
+            }, 3000);
+        }, 150);
+    }, 3000);
+}, 6300);
+*/
+setInterval(function () {
+    setTimeout(function (){document.getElementById("logo_name_after_logo").textContent = "b57ae478d"}, 2850);
+    setTimeout(function (){document.getElementById("logo_name_after_logo").textContent = "Юамбль"}, 3000);
+    setTimeout(function (){document.getElementById("logo_name_after_logo").textContent = "e48ed17e2"}, 5850);
+    setTimeout(function (){document.getElementById("logo_name_after_logo").textContent = "ㄚ"}, 6000);
+    setTimeout(function (){document.getElementById("logo_name_after_logo").textContent = "e46b19e4c"}, 8850);
+    setTimeout(function (){document.getElementById("logo_name_after_logo").textContent = "Yuamble"}, 9000);
+}, 9000);
+
